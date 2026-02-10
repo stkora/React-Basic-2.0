20e0fc7d3811f0f5dc461ffc2dc92f1955cbba8d
Rajib Soren <rajibsaren35@gmail.com>
2026-02-10 18:56:04 +0530

    my first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Subscription from './components/Subscription';
+
+function App() {
+  return (
+    <div className='App'>
+    <Subscription/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Subscription.js
@@ -0,0 +1,7 @@
+function Subscription () {
+    return (
+        <h2>Hello World!</h2>
+    )
+}
+
+export default Subscription;
