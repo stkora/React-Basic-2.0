a9c519576fe9ff9644ee7459b53094b0d3ce02d2
Rajib Soren <rajibsaren35@gmail.com>
2026-02-11 08:12:04 +0530

    learn dynamic data

--- my-app/src/components/Subscription.js
+++ my-app/src/components/Subscription.js
@@ -1,6 +1,24 @@
+import './Subscription.css'
+
 function Subscription () {
+
+    //dynamic data
+
+    let date = new Date('2026','02','08');
+    let title = 'MonthlynSubscription'
+    let amount = '125.60'
+
     return (
-        <h2>Hello World!</h2>
+        <div className='subscription'>
+
+        <div>{date.toISOString()}</div>
+        <h2 className='subscription_title'>{title}</h2>
+        <div className='subscription_price'>{amount}</div>
+
+        </div>
+       
+
+       
     )
 }
 
