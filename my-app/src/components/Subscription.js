1293fe6fbb2170166f85ca57b547f280af6221df
Rajib Soren <rajibsaren35@gmail.com>
2026-02-17 08:25:02 +0530

    onClick event listining

--- my-app/src/components/Subscription.js
+++ my-app/src/components/Subscription.js
@@ -3,6 +3,12 @@ import Container from '../templates/Container';
 import './Subscription.css'
 import SubscriptionDate from './SubscriptionDate';
  const Subscription = (props)=> {
+    let title = props.title
+    const onClickHandler =() =>{}
+    
+    title = "Changed Title";
+    console.log("On Button Clicked");
+    
 
     return (
         <Container className='subscription'>
@@ -10,7 +16,7 @@ import SubscriptionDate from './SubscriptionDate';
         <SubscriptionDate date={props.date}/>
         <h2 className='subscription_title'>{props.title}</h2>
         <div className='subscription_price'>{props.amount}</div>
-        <button type='button' onClick="">
+        <button type='button' onClick={onClickHandler()}>
         Change Title</button>
 
         </Container>
