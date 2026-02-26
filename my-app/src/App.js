10bfe22cb969db3b7f07d7d1e93a6ea5130f3f9e
Rajib Soren <rajibsaren35@gmail.com>
2026-02-26 14:40:51 +0530

    filter and condition in react application

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -50,13 +50,32 @@ const INITIAL_SUBSCRIPTION = [
   // React.createElement('div',{},React.createElement('h2',{},'Lets start!'))
     <Container>
 
-      <NewSubscription onAddSubscription = {addSubscriptionHandler}  />
+    <NewSubscription onAddSubscription = {addSubscriptionHandler}  />
       
     <Filter onFilterChange={filterChangeHandler}   selectedFilter={filteredYear}/>
-    {filteredSubscriptions.map((subscription)=>
+    {filteredSubscriptions.length===0 && <h3>No Data Found!</h3>}
+    {filteredSubscriptions.length !== 0 &&
+    filteredSubscriptions.map((subscription)=>
     <Subscription key={subscription.id} date={subscription.date} 
     title={subscription.title} amount={subscription.amount}/>)}
     
+    
+
+    {/* {filteredSubscriptions.length===0 ? <h3> No Data Found!</h3>:
+    filteredSubscriptions.map((subscription)=>
+    <Subscription key={subscription.id} date={subscription.date} 
+    title={subscription.title} amount={subscription.amount}/>)
+    } */}
+
+
+
+    {/* {filteredSubscriptions.map((subscription)=>
+    <Subscription key={subscription.id} date={subscription.date} 
+    title={subscription.title} amount={subscription.amount}/>)
+    } */}
+
+
+
     {/* <Subscription date={subscriptions[0].date}  title={subscriptions[0].title}  amount={subscriptions[0].amount}/>
     <Subscription date={subscriptions[1].date}  title={subscriptions[1].title}  amount={subscriptions[1].amount}/>
     <Subscription date={subscriptions[2].date}  title={subscriptions[2].title}  amount={subscriptions[2].amount}/>    */}
