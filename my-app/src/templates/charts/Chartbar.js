2ee438008b657aa6d75fad44ca43a0ba350d1dce
Rajib Soren <rajibsaren35@gmail.com>
2026-03-02 15:35:08 +0530

    learn about charts and its application

---
+++ my-app/src/templates/charts/Chartbar.js
@@ -0,0 +1,21 @@
+import './Chartbar.css';
+const Chartbar = (props) => {
+    let barHeight= "0%"; 
+    if(props.maxValue>0) {
+        barHeight.match.round((props.value/props.maxValue)*100)+"%";
+    }
+    return (
+        <div className='chart-bar'>
+            <div className='chart-bar_inner'>
+                <div className='chart-bar_fill' style={{'height':barHeight}}>
+
+                </div>
+
+            </div>
+            <div className='chart-bar_label'>{props.label}</div>
+        </div>
+    )
+
+
+}
+export default Chartbar;
